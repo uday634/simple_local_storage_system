2007bc5f12dd011eb6e9a28899a99694ab11ee5e
uday634 <uk8096386837@gmail.com>
2023-08-12 11:40:41 +0530

    2 commit

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
-var name1 = document.getElementById('name')
-var email = document.getElementById('email')
-var btn = document.getElementById('button')
-var number = document.getElementById('phone')
-var list = document.getElementById('list');     
+var name1 = document.getElementById("name");
+var email = document.getElementById("email");
+var btn = document.getElementById("button");
+var number = document.getElementById("phone");
+var list = document.getElementById("list");
 
-name1.addEventListener('keyup',(e)=>{
-    var name_val = e.target.value
-    console.log(name_val)
-})
-email.addEventListener('keyup',(e)=>{
-    var email_val = e.target.value
-    console.log(email_val)
-})
-number.addEventListener('keyup',(e)=>{
-    var number_val = e.target.value
-    console.log(number_val)
-})
-
-btn.addEventListener('click',(e)=>{
-    let li = document.createElement('li');
-    li.innerText = name1.value + '-' + email.value+'-'+number.value;
-    list.appendChild(li);
-    var obj = {
-        name:name1.value,
-        email:email.value,
-        phonenumber:number.value
-    }
-    let newObj = JSON.stringify(obj)
-    localStorage.setItem(email.value,newObj)
+name1.addEventListener("keyup", (e) => {
+  var name_val = e.target.value;
 });
+email.addEventListener("keyup", (e) => {
+  var email_val = e.target.value;
+});
+number.addEventListener("keyup", (e) => {
+  var number_val = e.target.value;
+});
+
+btn.addEventListener("click", (e) => {
+    var li = document.createElement("li");
+    var delbtn = document.createElement("button");
+    delbtn.innerText = "Delete";
+  
+    var emailValue = email.value; 
+  
+    li.setAttribute("data-email", emailValue); 
+    li.innerText = name1.value + "-" + emailValue + "-" + number.value;
+  
+    list.appendChild(li);
+    li.appendChild(delbtn);
+    var obj = {
+      name: name1.value,
+      email: emailValue,
+      phonenumber: number.value,
+    };
+    let newObj = JSON.stringify(obj);
+    localStorage.setItem(emailValue, newObj);
+  
+    delbtn.addEventListener("click", (e) => {
+      var emailToRemove = li.getAttribute("data-email");
+      list.removeChild(li);
+      localStorage.removeItem(emailToRemove); 
+    });
+  });
+  
