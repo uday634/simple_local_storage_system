4013db9cd956a3c66919e1b1fe360927a1c4556d
uday634 <uk8096386837@gmail.com>
2023-08-29 19:48:54 +0530

    5 commit

--- script.js
+++ script.js
@@ -1,68 +1,75 @@
 var name1 = document.getElementById("name");
-var email = document.getElementById("email");
+var email1 = document.getElementById("email");
 var btn = document.getElementById("button");
 var number = document.getElementById("phone");
 var list = document.getElementById("list");
 var container = document.getElementById("container");
+  //fetching the data
+axios
+  .get('https://crudcrud.com/api/a5c9153548244b31a6b0cb44e73daad8/AppData')
+  .then((response)=>{
+    var data = response.data;
+    console.log(data)
+    for(var i =0; i<data.length;i++){
+      var email = data[i].email;
+      var name = data[i].name;
+      var phonenumber = data[i].phonenumber;
+      var id = data[i]._id;
+      creatingList(name, email, phonenumber,id )
+    }
+  })
+  .catch((err)=>console.error(err))
 
-name1.addEventListener("keyup", (e) => {
-  var name_val = e.target.value;
-});
-email.addEventListener("keyup", (e) => {
-  var email_val = e.target.value;
-});
-number.addEventListener("keyup", (e) => {
-  var number_val = e.target.value;
-});
-
-btn.addEventListener("click", (e) => {
+//creating the list
+function creatingList(name1value, emailValue, numbervalue, id ){
   var li = document.createElement("li");
   var delbtn = document.createElement("button");
   var edit = document.createElement("button");
+  delbtn.addEventListener('click',(e)=>{
+    axios
+    .delete('https://crudcrud.com/api/a5c9153548244b31a6b0cb44e73daad8/AppData/'+id)
+    .then((res)=>console.log(res))
+    .catch((err)=>console.log(err))
+    li.remove()
+  })
   delbtn.innerText = "Delete";
   edit.innerText = "edit";
 
-  var emailValue = email.value;
-
+  li.setAttribute('id',id)
   li.setAttribute("data-email", emailValue);
-  li.setAttribute("data-name", name1.value);
-  li.setAttribute("data-number", number.value);
-  li.innerText = name1.value + "-" + emailValue + "-" + number.value;
+  li.setAttribute("data-name", name1value);
+  li.setAttribute("data-number", numbervalue);
+  li.innerText = name1value + "-" + emailValue + "-" + numbervalue;
 
   list.appendChild(li);
   li.appendChild(delbtn);
   li.appendChild(edit);
+}
+
+
+btn.addEventListener("click", (e) => {
+  var name = name1.value;
+  var email = email1.value;
+  var phone = number.value
+  
   var obj = {
-    name: name1.value,
-    email: emailValue,
-    phonenumber: number.value,
+    name: name,
+    email: email,
+    phonenumber: phone,
   };
-  let newObj = JSON.stringify(obj);
   axios
     .post(
-      "https://crudcrud.com/api/fe03d5caab3548adb8278ee7c1838a3b/AppointmentData",
+      "https://crudcrud.com/api/a5c9153548244b31a6b0cb44e73daad8/AppData",
       obj
     )
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
-
-  delbtn.addEventListener("click", (e) => {
-    var emailToRemove = li.getAttribute("data-email");
-    list.removeChild(li);
-    localStorage.removeItem(emailToRemove);
-  });
-  edit.addEventListener("click", (e) => {
-    name1.value = li.getAttribute("data-name");
-    email.value = li.getAttribute("data-email");
-    number.value = li.getAttribute("data-number");
-    var emailToRemove = li.getAttribute("data-email");
-  });
-
-  axios
-  .get('https://crudcrud.com/api/fe03d5caab3548adb8278ee7c1838a3b/AppointmentData')
-  .then((response)=>console.log(response))
-  .catch((err)=>console.error(err))
-
+    .then((res)=>{
+      var id = res.data._id;
+      creatingList(name, email, phonenumber,id )
+    })
+    .catch();
 
 });
+
+
+
 
